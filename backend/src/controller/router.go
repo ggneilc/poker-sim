8d8edeab73a17301ea6c80922574c0e4f6a11741
ggneilc <nchurro25@proton.me>
2024-01-11 17:41:23 -0600

    added websocket to server

--- backend/src/controller/router.go
+++ backend/src/controller/router.go
@@ -2,9 +2,11 @@
 package controller
 
 import (
+	"log"
 	"math/rand"
 	"time"
 
+	"github.com/gofiber/contrib/websocket"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -12,7 +14,34 @@ func SetupRoutes(app *fiber.App) {
 
 	app.Get("/home", displayHome)
 	app.Get("/new", generateGame)
+	app.Get("/ws/:id", websocket.New(establishWebsocket))
+}
 
+func establishWebsocket(c *websocket.Conn) {
+	// c.Locals is added to the *websocket.Conn
+	log.Println(c.Locals("allowed"))  // true
+	log.Println(c.Params("id"))       // 123
+	log.Println(c.Query("v"))         // 1.0
+	log.Println(c.Cookies("session")) // ""
+
+	// websocket.Conn bindings https://pkg.go.dev/github.com/fasthttp/websocket?tab=doc#pkg-index
+	var (
+		mt  int
+		msg []byte
+		err error
+	)
+	for {
+		if mt, msg, err = c.ReadMessage(); err != nil {
+			log.Println("read:", err)
+			break
+		}
+		log.Printf("recv: %s", msg)
+
+		if err = c.WriteMessage(mt, msg); err != nil {
+			log.Println("write:", err)
+			break
+		}
+	}
 }
 
 func displayHome(c *fiber.Ctx) error {
--- backend/src/main.go
+++ backend/src/main.go
@@ -5,11 +5,22 @@ import (
 	"log"
 
 	"github.com/ggneilc/poker-sim/src/controller"
+	"github.com/gofiber/contrib/websocket"
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	app := fiber.New()
+
+	app.Use("/ws", func(c *fiber.Ctx) error {
+		// IsWebSocketUpgrade returns true if the client
+		// requested upgrade to the WebSocket protocol.
+		if websocket.IsWebSocketUpgrade(c) {
+			c.Locals("allowed", true)
+			return c.Next()
+		}
+		return fiber.ErrUpgradeRequired
+	})
 
 	app.Static("/", "../../frontend")
 
