bfab9b3ffc876137cfa1eb540b71ddafd32ce4bc
ggneilc <nchurro25@proton.me>
2024-07-30 22:42:01 -0500

    fixed bj templates structure

--- gopoker/gopoker/settings.py
+++ gopoker/gopoker/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -56,7 +55,7 @@ ROOT_URLCONF = 'gopoker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [ BASE_DIR / 'templates'],
+        'DIRS': [ BASE_DIR / 'templates'], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
