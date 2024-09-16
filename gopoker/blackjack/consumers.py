f6aeadebe198a93320a10ae2cb54b1e76417cdb6
ggneilc <nchurro25@proton.me>
2024-09-16 18:34:04 -0500

    implemented websocket event: hit/stand

--- gopoker/blackjack/consumers.py
+++ gopoker/blackjack/consumers.py
@@ -1,4 +1,6 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
+from django.template.loader import render_to_string
+from .utils import Card
 import json
 
 
@@ -27,21 +29,62 @@ class BlackjackConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         '''receive message'''
         data = json.loads(text_data)
-        event_type = data.get('event_type')
-        player = data.get('player')
+        message_type = data.get('type')
+        if message_type == 'player_action':
+            event_type = data.get('event_type')
+            user = data.get('user')
 
-        # process different events
-        if event_type == "hit":
-            await self.handle_hit(player)
-        elif event_type == "stand":
-            await self.handle_stand(player)
+            # process different events
+            if event_type == "hit":
+                await self.handle_hit(user)
+            elif event_type == "stand":
+                await self.handle_stand(user)
 
     async def handle_hit(self, player):
         '''handle a player hitting'''
+        card = Card.newCard()
+        context = {
+            "target": 'player',
+            "num": card.getNum(),
+            "suit": card.getSuit(),
+            "card": card.toString()
+        }
+        html = render_to_string('blackjack/card.html', context)
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'html_message',
+                'html': html
+            }
+        )
 
     async def handle_stand(self, player):
         '''handle a player standing'''
+        card = Card.newCard()
+        context = {
+            "target": 'dealer',
+            "num": card.getNum(),
+            "suit": card.getSuit(),
+            "card": card.toString()
+        }
+        html = render_to_string('blackjack/card.html', context)
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'html_message',
+                'html': html
+            }
+        )
 
     async def game_message(self, event):
         '''send message to websocket'''
-        await self.send(text_data=json.jumps(event['message']))
+        await self.send(text_data=json.dumps(event['message']))
+
+    async def html_message(self, event):
+        '''sends html to the websocket client'''
+        await self.send(text_data=event['html'])
+#        await self.send(text_data=json.dumps({
+#            "field": event['html']
+#        }))
