d163bdb49049e1f0ad2f50f90f1064623f28aec7
ggneilc <nchurro25@proton.me>
2024-10-18 19:46:17 -0500

    configured static files for Heroku

--- gopoker/gopoker/settings.py
+++ gopoker/gopoker/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Static Files Storage
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -161,6 +162,8 @@ STATICFILES_DIRS = [
 
 # The folder where Django will collect static files for deployment
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# For Heroku specifically
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
