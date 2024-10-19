bb32f844472f505711554fb0477b1de3b8405204
ggneilc <nchurro25@proton.me>
2024-10-18 19:42:52 -0500

    static files for deployment

--- gopoker/gopoker/settings.py
+++ gopoker/gopoker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -157,6 +158,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
         BASE_DIR / "static",
 ]
+
+# The folder where Django will collect static files for deployment
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
